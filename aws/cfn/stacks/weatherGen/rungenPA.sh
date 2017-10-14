b7b5a5f750b3342d22c66219c8da47abe9e3cb7d
arindamsinha12 <32620042+arindamsinha12@users.noreply.github.com>
2017-10-14 16:24:20 -0700

    Update rungenPA.sh

--- aws/cfn/stacks/weatherGen/rungenPA.sh
+++ aws/cfn/stacks/weatherGen/rungenPA.sh
@@ -1,1 +1,1 @@
-node ./generate.js --state "California" --city "Palo Alto" --longtitude "-122.142776" --latitude "37.399782" --sensor "sensor_1"
+node ./generate.js --state "California" --city "Palo Alto" --longtitude "-122.142776" --latitude "37.399782" --sensor "sensor_PA"
