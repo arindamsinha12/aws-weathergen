b4b256586060607b2d2719a149f243b7060d4a43
arindamsinha12 <32620042+arindamsinha12@users.noreply.github.com>
2017-10-21 14:08:18 -0700

    Update and rename rungenSE.sh to rungenSJ.sh

--- aws/cfn/stacks/weatherGen/rungenSE.sh
+++
@@ -1,1 +0,0 @@
-node ./generate.js --state "Washington" --city "Seattle" --longtitude "-122.335167" --latitude "47.608013" --sensor "sensor_SE"
---
+++ aws/cfn/stacks/weatherGen/rungenSJ.sh
@@ -0,0 +1,1 @@
+node ./generate.js --state "California" --city "San Jose" --longtitude "-121.867905" --latitude "37.279518" --sensor "sensor_SJ"
