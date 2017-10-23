7623edfdaae5dcd920f8941399d4364d0b12be2b
arindamsinha12 <32620042+arindamsinha12@users.noreply.github.com>
2017-10-23 13:16:38 -0700

    Update rungenSJ.sh

--- aws/cfn/stacks/weatherGen/rungenSJ.sh
+++ aws/cfn/stacks/weatherGen/rungenSJ.sh
@@ -1,1 +1,1 @@
-node ./generate.js --state "California" --city "San Jose" --longtitude "-121.867905" --latitude "37.279518" --sensor "sensor_SJ"
+node /aws-weathergen/generate.js --state "California" --city "San Jose" --longtitude "-121.867905" --latitude "37.279518" --sensor "sensor_SJ"
